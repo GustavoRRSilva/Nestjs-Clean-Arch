d91dd2f39f6043b651529bec74871a9db90c67ff
Gustavo <gstvrrds@gmail.com>
2025-06-28 05:30:55 -0300

    feat:addign user resource and new directories

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module'
 import { ConfigModule } from '@nestjs/config'
+import { UsersModule } from './users/infrastructure/users.module'
 
 @Module({
-  imports: [EnvConfigModule, ConfigModule],
+  imports: [EnvConfigModule, ConfigModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
